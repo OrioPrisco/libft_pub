c69234d41c13a57c73325f6a14c0bf983b0e12ea
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-04-14 18:26:56 +0200

    fix ft_strtol_check_int
    the pointer substraction to check the string's length was done backward, so a string of length 20 would be interpreted as having length -20.

--- srcs/ft_strtol_check_int.c
+++ srcs/ft_strtol_check_int.c
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:49:33 by OrioPriscc        #+#    #+#             */
-/*   Updated: 2023/04/11 13:15:49 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/04/14 18:26:33 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ _Bool	ft_strtol_check_int(const char *str, const char *endptr, long parsed)
 	str = ft_next_non_space(str);
 	if (*str == '+' || *str == '-')
 		str++;
-	if (endptr && str - endptr > 10)
+	if (endptr && endptr - str > 10)
 		return (1);
 	if (parsed > INT_MAX || parsed < INT_MIN)
 		return (1);
