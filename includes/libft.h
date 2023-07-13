eb1e8a2abb91179f6eb64200c9d71b368e18e2d6
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-07-13 15:05:30 +0200

    fd_putstr_fd : take const char *
    this function doesn't modify the string, so there is no reason
    to not have the parameter be const

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:55:38 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2023/07/05 16:22:55 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2023/07/13 15:02:01 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ char		*ft_strtrim(const char *s, const char *set);
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char		*ft_striteri(char *s, void (*f)(unsigned int, char *));
 void		ft_putchar_fd(char c, int fd);
-void		ft_putstr_fd(char *s, int fd);
+void		ft_putstr_fd(const char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 char		**ft_split(char const *s, char c);
--- srcs/ft_putstr_fd.c
+++ srcs/ft_putstr_fd.c
@@ -6,14 +6,14 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:50:52 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2023/04/11 13:15:48 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/07/13 15:01:46 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(const char *s, int fd)
 {
 	(void)(write(fd, s, ft_strlen(s)) + 1);
 }
