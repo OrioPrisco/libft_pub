30f02b7286ab6e6ed60ed0ebc53d39499f9658c6
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-07-28 18:19:11 +0200

    Pull ftprintf for ft_dprintf

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 15:58:34 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2023/04/11 13:15:46 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/07/28 18:18:01 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,5 @@
 # define FT_PRINTF_H
 
 int		ft_printf(const char *s, ...);
+int		ft_dprintf(int fd, const char *s, ...);
 #endif
